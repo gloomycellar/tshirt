fa057a9ac2ddeced807ce6c1a178cafd15c1c3af
tisa <timursayfullin@gmail.com>
2014-06-29 16:29:46 +0300

    ''

--- tshirt/controllers/main.js
+++ tshirt/controllers/main.js
@@ -1,10 +1,14 @@
 ﻿'use strict';
 
 angular.module('testAngularJsApp')
-    .controller('main', function ($scope, productService) {
+    .controller('main', function ($scope, $location, productService) {
         var promise = productService.getProducts();
         promise.then(function (data) {            
             $scope.tshirtsActive = data.splice(0, 4);
             $scope.tshirts = data;
         });
+
+        $scope.showDetails = function (id) {
+            $location.path("/productdetails/" + id);
+        };
     });
